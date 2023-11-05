29236f047fa65b9b417272b1b7ebba89ed19b52a
ml18xe <77432801+ml18xe@users.noreply.github.com>
2023-11-05 16:04:23 -0500

    added javascript

---
+++ myscripts.js
@@ -0,0 +1,8 @@
+function init(){
+    document.getElementById("question").innerHTML = "角";
+    document.getElementById("answer").innerHTML = "";
+}
+
+function revealAnswer(){
+    document.getElementById("answer").innerHTML = "かど corner";
+}
