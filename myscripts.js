1b7d00d4cfc579bf36d1b91dbf6e3a13e7d6ef80
ml18xe <77432801+ml18xe@users.noreply.github.com>
2023-11-06 02:34:37 -0500

    Added Navigation

--- myscripts.js
+++ myscripts.js
@@ -1,8 +1,52 @@
+var question;
+var answer;
+
 function init(){
-    document.getElementById("question").innerHTML = "角";
-    document.getElementById("answer").innerHTML = "";
+    question = "角";
+    answer = "かど - corner";
+    document.getElementById("question").innerHTML = question;
 }
 
 function revealAnswer(){
-    document.getElementById("answer").innerHTML = "かど - corner";
+    if(!document.getElementById("answer")){
+
+        //Answer
+        let x = document.createElement("p");
+        let t = document.createTextNode(answer);
+        
+        x.id = "answer";
+        x.className = "answer";
+        x.appendChild(t);
+        document.getElementById("center").appendChild(x);
+        
+        //Nav Buttons
+        let remainingText = document.createElement("p");
+        let incorrectButton = document.createElement("button");
+        let correctButton = document.createElement("button");
+        let container = document.createElement("div");
+
+        remainingText.className = "answer";
+        incorrectButton.className = "answer";
+        correctButton.className = "answer";
+        container.className = "bottom";
+
+        remainingText.appendChild(document.createTextNode("Remaining: 000"));
+        incorrectButton.appendChild(document.createTextNode("Incorrect: 000"));
+        correctButton.appendChild(document.createTextNode("Correct: 000"));
+        container.appendChild(remainingText);
+        container.appendChild(incorrectButton);
+        container.appendChild(correctButton);
+        
+        document.body.appendChild(container);
+
+    }
+
+    // x.onclick = function() {nextQuestion()};
+}
+
+function nextQuestion(){
+    question = "角度";
+    answer = "かくど - degree of an angle";
+    document.getElementById("question").innerHTML = question;
+    document.getElementById("answerButton").remove();
 }
